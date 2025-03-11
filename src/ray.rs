729ff5e180531868c41484e132e7241c447433aa
ManixI <ManixI915@gmail.com>
2025-03-10 17:28:30 -0700

    added method to calc position at time t to ray

--- src/ray.rs
+++ src/ray.rs
@@ -1,6 +1,6 @@
 use super::Coord;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug,PartialEq, Clone, Copy)]
 pub struct Ray {
     origin: Coord,
     direction: Coord
@@ -10,6 +10,10 @@ pub struct Ray {
 impl Ray {
     pub fn new(origin: Coord, direction: Coord) -> Self {
         Ray { origin, direction }
+    }
+
+    fn position(&self, time: f32) -> Coord {
+        self.origin + self.direction * time
     }
 }
 
@@ -24,5 +28,14 @@ mod tests {
         let r = Ray::new(o, d);
         assert_eq!(r.direction, d);
         assert_eq!(r.origin, o);
+    }
+
+    #[test]
+    fn test_position() {
+        let r = Ray::new(Coord::point(2.0, 3.0, 4.0), Coord::vec(1.0, 0.0, 0.0));
+        assert_eq!(r.position(0.0), Coord::point(2.0, 3.0, 4.0));
+        assert_eq!(r.position(1.0), Coord::point(3.0, 3.0, 4.0));
+        assert_eq!(r.position(-1.0), Coord::point(1.0, 3.0, 4.0));
+        assert_eq!(r.position(2.5), Coord::point(4.5, 3.0, 4.0));
     }
 }

789087f11b608c0e4e8b5f89236230c981fb035d
ManixI <manixi915@gmail.com>
2025-04-21 18:21:54 -0700

    removed origin in favor of transformation matrix

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,39 +1,56 @@
 use core::f32;
 use std::rc::Rc;
+use crate::matrix::Matrix;
 use crate::ray::{Intersect, Ray};
 use crate::ray::intersection::*;
 use super::Coord;
 
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Sphere {
-    origin: Coord,
+    //origin: Coord,
     radius: f32,
+    transformation: Matrix,
 }
 
 #[allow(dead_code)]
 impl Sphere {
     /// a sphere at position (0, 0, 0) with a radius of 1
     pub fn default() -> Self {
-        Self { origin: Coord::point(0.0, 0.0, 0.0), radius: 1.0 }
+        Self { radius: 1.0, transformation: Matrix::identity(4) }
     }
 
     pub fn new(origin: Coord, radius: f32) -> Self {
         assert!(origin.is_point());
-        Self {origin, radius}
+        Self {radius, transformation: Matrix::from_point(&origin)}
+    }
+
+    pub fn set_transformation(&mut self, mat: Matrix) {
+        self.transformation = mat;
+    }
+
+    // TODO: remove clone here
+    pub fn apply_transformation(&mut self, mat: Matrix) {
+        self.transformation = self.transformation.clone() * mat;
+    }
+
+    pub fn get_origin(&self) -> Coord {
+        self.transformation.to_point()
+    }
+
+    pub fn get_transformation(&self) -> Matrix {
+        self.transformation.clone()
     }
 
     pub fn geometric_intersect(&self, ray: &Ray) -> Option<[f32; 2]> {
         // ref: https://discussions.unity.com/t/how-do-i-find-the-closest-point-on-a-line/588895/3
         let dir = ray.get_direction();//.normalized();
-        let v = self.origin - ray.get_origin();
+        let v = self.get_origin() - ray.get_origin();
         let d = v.dot(dir);
-        //if d < 0 {
-        //    return None;
-        //}
-        println!("{} {}", d, self.radius);
+        
+        //println!("{} {}", d, self.radius);
         let nearest = ray.get_origin() + dir * d;
-        //let dist = nearest.len();
+        
         // better to square radius for comparison then sqrt the dist as dist isn't actually needed
         let dist = nearest.get_x().powi(2) + nearest.get_y().powi(2) + nearest.get_z().powi(2);
         //println!("dist: {:?}", dist);
@@ -69,7 +86,7 @@ impl Sphere {
     }
 
     pub fn analytical_intersect(&self, ray: &Ray) -> Option<[f32; 2]> {
-        let l = ray.get_origin() - self.origin;
+        let l = ray.get_origin() - self.get_origin();
         //let a = ray.get_direction().dot(ray.get_direction());
         let b = 2.0 * ray.get_norm_direction().dot(l);
         let c = l.dot(l) - self.radius.powi(2);
@@ -108,13 +125,33 @@ impl Intersect<Self> for Sphere {
             return None;
         }
         let data = data.unwrap();
-        let t = Rc::new(*self);
+        let t = Rc::new(self.clone());
         Some([Intersection::new(data[0], t.clone()), Intersection::new(data[1], t)])
     }
 }
 
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_sphere_creation() {
+        let s = Sphere::default();
+        assert_eq!(s.radius, 1.0);
+        assert_eq!(s.transformation, Matrix::identity(4));
+
+        let s = Sphere::new(Coord::point(0.0, 0.0, 0.0), 2.0);
+        assert_eq!(s.radius, 2.0);
+        assert_eq!(s.transformation, Matrix::identity(4));
+    }
+
+    #[test]
+    fn test_set_transformation() {
+        let mut s = Sphere::default();
+        let mat = Matrix::translation(2.0, 3.0, 4.0);
+        s.set_transformation(mat.clone());
+        assert_eq!(s.get_transformation(), mat);
+        assert_eq!(s.get_origin(), mat.to_point());
+    }
 
     #[test]
     fn test_sphere_intersection_no_position() {

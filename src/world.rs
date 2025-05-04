f9efb247d6882af0b14b50c99df7cf27ca04fe04
ManixI <manixi915@gmail.com>
2025-05-03 23:04:53 -0700

    added tests for World class

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,4 @@
-use crate::{canvas::color::Color, coord::Coord, light::Light, matrix::Matrix, ray::{intersection::Intersection, Ray}, renderable::Renderable, sphere::Sphere};
+use crate::{canvas::color::Color, coord::Coord, light::Light, material::Material, matrix::Matrix, ray::{intersection::Intersection, Ray}, renderable::Renderable, sphere::Sphere};
 
 // I'm going to need to re-work this to add all objects, not just renderable ones aren't I
 // probably just make a node type or something
@@ -19,8 +19,13 @@ impl World {
         let s1 = Sphere::new(Coord::point(0.0, 0.0, 0.0));
         let mut s2 = Sphere::new(Coord::point(0.0, 0.0, 0.0));
         s2.set_transformation(Matrix::scaling(0.5, 0.5, 0.5));
-        let s1 = Box::new(s1);
-        let objs = vec![s1 as Box<dyn Renderable>];
+        let mat = Material::new(0.2, 0.7, 0.2, 200.0, Color::new(0.8, 1.0, 0.6, 0.0));
+        s2.set_material(mat);        
+
+
+        let s1 = Box::new(s1) as Box<dyn Renderable>;
+        let s2 = Box::new(s2) as Box<dyn Renderable>;
+        let objs = vec![s1, s2];
         Self { light: Some(l), objects: objs }
     }
 
@@ -29,8 +34,8 @@ impl World {
     }
 
     fn get_object(&self) -> Vec<Box<dyn Renderable>> {
-        //self.objects.clone()
-        todo!()
+        self.objects.clone()
+        //todo!()
     }
 
     fn get_intersections(&self, ray: Ray) -> Vec<Intersection> {
@@ -42,5 +47,43 @@ impl World {
 
 #[cfg(test)]
 mod tests {
+    use crate::{canvas::color::Color, coord::Coord, light::Light, material::Material, matrix::Matrix, renderable::{compare_renderables, Renderable}, sphere::Sphere};
+
+    use super::World;
+
+
+    #[test]
+    fn test_new() {
+        let w = World::new();
+        assert!(w.light.is_none());
+        assert_eq!(w.objects.len(), 0);
+    }
+
+    #[test]
+    fn test_getters() {
+        let w = World::new();
+        assert!(w.get_light().is_none());
+        assert_eq!(w.get_object().len(), 0);
+    }
+
+    #[test]
+    fn test_default() {
+        let w = World::default();
+        
+        assert!(w.get_light().is_some());
+        assert_eq!(w.get_light().unwrap(), Light::new(Coord::point(-10.0, 10.0, -10.0), Color::white()));
+        
+        let s1 = Sphere::default();
+        let objs = w.get_object();
+        assert_eq!(objs.len(), 2);
+        assert!(compare_renderables(objs[0].as_ref(), &s1));
+
+        let mut s2 = Sphere::default();
+        let mat = Material::new(0.2, 0.7, 0.2, 200.0, Color::new(0.8, 1.0, 0.6, 0.0));
+        s2.set_material(mat);
+        s2.set_transformation(Matrix::scaling(0.5, 0.5, 0.5));
+
+        assert!(compare_renderables(objs[1].as_ref(), &s2));
+    }
 
 }

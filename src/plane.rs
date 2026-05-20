1f075f9275cd30ee8232b0981d8b1bf8ed0f28dd
ManixI <manixi915@gmail.com>
2026-05-19 23:06:57 -0700

    added some plane tests

--- src/plane.rs
+++ src/plane.rs
@@ -27,6 +27,9 @@ impl Renderable for Plane {
     }
 
     fn intersect_get_ray(&self, ray: Ray) -> (Ray, Option<[Intersection; 2]>) {
+        if ray.get_direction().get_y().abs() - 0.0001 > 0.0 {
+            return (ray, None);
+        }
         todo!()
     }
 
@@ -43,8 +46,28 @@ impl_renderable_tests!(crate::plane::Plane, RenderableType::Plane);
 
 #[cfg(test)]
 mod tests {
-    use crate::plane;
     use super::*;
 
+    #[test]
+    fn test_normal() {
+        let p = Plane::default();
+        let n1 = p.normal_at(Coord::point(0.0, 0.0, 0.0));
+        let n2 = p.normal_at(Coord::point(10.0, 0.0, -10.0));
+        let n3 = p.normal_at(Coord::point(-5.0, 0.0, 150.0));
+        let tn = Coord::vec(0.0, 1.0, 0.0);
+        assert_eq!(n1, tn);
+        assert_eq!(n2, tn);
+        assert_eq!(n3, tn);
+    }
     
+    #[test]
+    fn test_ray_intersect_above() {
+        let p = Plane::default();
+        let r = Ray::new(Coord::point(0.0, 1.0, 0.0), Coord::vec(0.0, -1.0, 0.0));
+        let xs = p.intersect(r);
+        let xs = Intersection::aggregate_intersections(vec![xs]);
+        assert_eq!(xs.len(), 1);
+        assert_eq!(xs[0].get_time(), 1.0);
+        assert_eq!(xs[0].get_object(), p);
+    }
 }
